294b85536bec49c385425c2d81f3a04ae9ad95a8
Monique McIntyre <skaiwilliams85@gmail.com>
2022-02-26 23:45:08 -0500

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./App.css";
+import Navigation from "./components/nav/Navigation";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/nav/Navigation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "./Navigation.module.css";
+
+const Navigation = () => {
+  return <nav className={styled.navbar}></nav>;
+};
+
+export default Navigation;
