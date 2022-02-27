8314ba61a945e1a5f44c950ac8b117ffad1be878
Monique McIntyre <skaiwilliams85@gmail.com>
2022-02-27 08:11:13 -0500

    added showForm functionality

--- src/components/notes/Form.js
+++ src/components/notes/Form.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Dropdown from "./Dropdown";
 import styled from "./Form.module.css";
 
 const Form = () => {
+  const [showForm, setShowForm] = useState(false);
+  const ref = useRef();
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  useEffect(() => {
+    const clickedOutside = (e) => {
+      //* If the section is open and the clicked target is not within the menu,then close the section
+      if (showForm && ref.current && !ref.current.contains(e.target)) {
+        setShowForm(false);
+      }
+    };
+
+    document.addEventListener("mousedown", clickedOutside);
+
+    return () => {
+      //* Remove event listener with cleanup function
+      document.removeEventListener("mousedown", clickedOutside);
+    };
+  }, [showForm]);
+
   return (
-    <section className={styled.formWrap}>
+    <section ref={ref} className={styled.formWrap} onClick={showFormHandler}>
       <p> Add new note</p>
-      <form>
-        <label htmlFor="title">Title</label>
-        <input type="text" name="title" placeholder="Title your note" />
 
-        <label htmlFor="">Note</label>
-        <input type="text" name="note" placeholder="Jot it down here..." />
+      {showForm && (
+        <form>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="Title your note" />
 
-        <Dropdown />
+          <label htmlFor="">Note</label>
+          <input type="text" name="note" placeholder="Jot it down here..." />
 
-        <button type="submit" className={styled.btnSubmit}>
-          Submit
-        </button>
-      </form>
+          <Dropdown />
+
+          <button type="submit" className={styled.btnSubmit}>
+            Submit
+          </button>
+        </form>
+      )}
     </section>
   );
 };
